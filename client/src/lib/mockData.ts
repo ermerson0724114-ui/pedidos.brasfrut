c32102e21eab311ed2fc49a2528b0f87e26adf4e
ermerson0724114-ui <ermerson0724114@gmail.com>
2026-03-02 13:17:18 -0300

    Delete client directory

--- client/src/lib/mockData.ts
+++
@@ -1,2 +0,0 @@
-export const MONTHS = ["Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"];
-export const MONTHS_FULL = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
